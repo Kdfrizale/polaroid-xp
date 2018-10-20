405598a02e08f3e9f908158d191bc41065e4118e
Kyle <kyle.frizzell.14@cnu.edu>
2018-10-20 00:07:19 -0400

    ADD Glide, IMPROVE Gallery Performance

--- app/src/main/java/frizzell/flores/polaroidxp/GalleryActivity.java
+++ app/src/main/java/frizzell/flores/polaroidxp/GalleryActivity.java
@@ -7,6 +7,7 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 
 import java.io.File;
@@ -27,8 +28,12 @@ public class GalleryActivity extends AppCompatActivity {
         recyclerView.setDrawingCacheEnabled(true);
         recyclerView.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_LOW);
 
-        ArrayList<GalleryItemModel> galleryImages = prepareData();
-        MyAdapter adapter = new MyAdapter(getApplicationContext(), galleryImages);
+        //ArrayList<GalleryItemModel> galleryImages = prepareData();
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"polaroidXP");
+        String path = storageDir.toString();
+        File f = new File(path);
+        File file[] = f.listFiles();
+        MyAdapter adapter = new MyAdapter(getApplicationContext(), file);
         recyclerView.setAdapter(adapter);
 
 
@@ -42,10 +47,33 @@ public class GalleryActivity extends AppCompatActivity {
         File f = new File(path);
         File file[] = f.listFiles();
         for (int i=0; i <file.length;i++){
+            long startTime = System.currentTimeMillis();
+            Log.e("Prepare Data", "loading a file...");
+            //TODO improve performance here
             GalleryItemModel itemModel = new GalleryItemModel();
-            itemModel.setImg(BitmapFactory.decodeFile(file[i].getAbsolutePath()));
+            long endTime = System.currentTimeMillis();
+            Log.e("TAG crete OBject","That took " + (endTime - startTime) + " milliseconds");
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeFile(file[i].getAbsolutePath(),options);
+            Log.d("Height:", Integer.toString(options.outHeight));
+            Log.d("width:", Integer.toString(options.outWidth));
+            options.inJustDecodeBounds = false;
+
+            options.inSampleSize = calculateInSampleSize(options, 400, 200);
+
+
+            endTime = System.currentTimeMillis();
+            Log.e("TAG heightwidth","That took " + (endTime - startTime) + " milliseconds");
+            itemModel.setImg(BitmapFactory.decodeFile(file[i].getAbsolutePath(),options));
+
+            endTime = System.currentTimeMillis();
+            Log.e("TAG After Bitmapfactory","That took " + (endTime - startTime) + " milliseconds");
 
             images.add(itemModel);
+            endTime = System.currentTimeMillis();
+            Log.e("TAG Total Time","That took " + (endTime - startTime) + " milliseconds");
             //GalleryItemModel createList = new GalleryItemModel();
             //createList.setImage_Path(file[i].getAbsolutePath());
             //createList.setImage_title(file[i].getName());
@@ -62,6 +90,28 @@ public class GalleryActivity extends AppCompatActivity {
         return images;
     }
 
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
 }
 
 
--- app/src/main/java/frizzell/flores/polaroidxp/MyAdapter.java
+++ app/src/main/java/frizzell/flores/polaroidxp/MyAdapter.java
@@ -13,15 +13,20 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+
 import org.w3c.dom.Text;
 
+import java.io.File;
 import java.util.ArrayList;
 
 class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-    private ArrayList<GalleryItemModel> galleryList;
+    //private ArrayList<GalleryItemModel> galleryList;
+    private File[] galleryList;
     private Context context;
+    private View myView;
 
-    public MyAdapter(Context context, ArrayList<GalleryItemModel> galleryList){
+    public MyAdapter(Context context, File[] galleryList){
         this.galleryList = galleryList;
         this.context = context;
     }
@@ -29,19 +34,22 @@ class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     @Override
     public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i){
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cell_layout,  viewGroup,false);
+        this.myView = view;
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(MyAdapter.ViewHolder viewHolder, int i){
         //viewHolder.title.setText(galleryList.get(i).getImage_title());
-        viewHolder.img.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        //viewHolder.img.setScaleType(ImageView.ScaleType.CENTER_CROP);
         Log.e("TAG:", "Helloooo");
+        File image = galleryList[i];
+        Glide.with(this.myView).load(image).into(viewHolder.img);
 //        Bitmap bmp = BitmapFactory.decodeFile(galleryList.get(i));
 //        int origWidth = bmp.getWidth();
 //        int origHeight = bmp.getHeight();
 //        bmp = Bitmap.createScaledBitmap(bmp, origWidth / 10, origHeight / 10, false);
-        viewHolder.img.setImageBitmap(galleryList.get(i).getImg());
+        //viewHolder.img.setImageBitmap(galleryList.get(i).getImg());
         //viewHolder.img.setImageResource((galleryList.get(i).getImage_ID()));
 //        viewHolder.img.setOnClickListener(new View.OnClickListener() {
 //            @Override
@@ -53,7 +61,7 @@ class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
     @Override
     public int getItemCount(){
-        return galleryList.size();
+        return galleryList.length;
     }
 
 
