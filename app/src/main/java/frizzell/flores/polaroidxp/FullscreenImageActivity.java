ad8ee4be2d4d02b3e75ac28f413796788ca22870
Kyle <kyle.frizzell.14@cnu.edu>
2018-11-03 20:47:32 -0400

    IMPROVE fullscreen image preview

--- app/src/main/java/frizzell/flores/polaroidxp/FullscreenImageActivity.java
+++ app/src/main/java/frizzell/flores/polaroidxp/FullscreenImageActivity.java
@@ -1,6 +1,9 @@
 package frizzell.flores.polaroidxp;
 
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -21,7 +24,30 @@ public class FullscreenImageActivity extends AppCompatActivity {
         File passedImage = (File) getIntent().getExtras().get("ImageFile");
         if(passedImage.exists()){
             Log.e("Fullscreen","Image received");
-            mImageView.setImageBitmap(BitmapFactory.decodeFile(passedImage.getAbsolutePath()));
+            try{
+                ExifInterface exif = new ExifInterface(passedImage.getAbsolutePath());
+                int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,1);
+                Log.d("EXIF initial", "Exif: " + orientation);
+                Matrix matrix = new Matrix();
+                if (orientation == 6) {
+                    matrix.postRotate(90);
+                    Log.d("EXIF", "Exif: " + orientation);
+                } else if (orientation == 3) {
+                    matrix.postRotate(180);
+                    Log.d("EXIF", "Exif: " + orientation);
+                } else if (orientation == 8) {
+                    matrix.postRotate(270);
+                    Log.d("EXIF", "Exif: " + orientation);
+                }
+                Bitmap bitmapSelectedImage = BitmapFactory.decodeFile(passedImage.getAbsolutePath());
+                bitmapSelectedImage = Bitmap.createBitmap(bitmapSelectedImage,0,0,bitmapSelectedImage.getWidth(),bitmapSelectedImage.getHeight(),matrix,true);
+                mImageView.setImageBitmap(bitmapSelectedImage);
+            }catch (Exception e){
+                Log.e("Fullscreen Exif", "Error");
+                e.printStackTrace();
+            }
+
+
         }
         else{
             Log.e("Fullscreen","File did not exist");
