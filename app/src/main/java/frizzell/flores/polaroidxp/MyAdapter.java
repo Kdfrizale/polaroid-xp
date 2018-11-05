73bdf9c711cdc1ddc63327803e2b4f2d80592a87
Kyle <kyle.frizzell.14@cnu.edu>
2018-11-04 21:17:07 -0500

    Fix crash caused by sending fulslcreenImagePreview intent from outside of activity

--- app/src/main/java/frizzell/flores/polaroidxp/MyAdapter.java
+++ app/src/main/java/frizzell/flores/polaroidxp/MyAdapter.java
@@ -62,6 +62,7 @@ class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
                 Intent fullscreenImageIntent = new Intent(context, FullscreenImageActivity.class);
                 //TODO we can save bandwidth by changing this to a string file name as that's all we are using
                 fullscreenImageIntent.putExtra("ImageFile", image);
+                fullscreenImageIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 context.startActivity(fullscreenImageIntent);
             }
 
